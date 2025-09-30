631e5831386c6f341c96b741fd52b391fedc52c1
bread <thayer2392@gmail.com>
2025-09-30 12:07:50 +0900

    feat: client 모듈 임포트

---
+++ clients/client-example/src/main/kotlin/com/rockpay/client/example/ExampleConfig.kt
@@ -0,0 +1,8 @@
+package com.rockpay.client.example
+
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Configuration
+
+@EnableFeignClients
+@Configuration
+internal class ExampleConfig
---
+++ clients/client-example/src/main/kotlin/com/rockpay/client/example/ExampleRequestDto.kt
@@ -0,0 +1,5 @@
+package com.rockpay.client.example
+
+internal data class ExampleRequestDto(
+    val exampleRequestValue: String,
+)
